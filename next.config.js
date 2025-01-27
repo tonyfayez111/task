1cfa996c0baf3dfec74aa4317b50d9cd08fb2fb9
tonyfayez111 <tonyfayez111@gmail.com>
2025-01-28 00:07:15 +0200

    next

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {  output: 'export',
+
+const nextConfig = {
+  output: 'export',
   distDir: 'out',
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
-},
-compiler: {
-  styledComponents: true,
-},
+  },
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  
   assetPrefix: isProd ? '/task' : '',
   basePath: isProd ? '/task' : '',
   webpack(config, options) {
